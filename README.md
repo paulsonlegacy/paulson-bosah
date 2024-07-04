0dec10a453eb2f7116786a69c8efd96d5827a136
Paulson Bosah <paulsonbosah@gmail.com>
2024-07-04 17:59:12 +0100

    Update commit

--- README.md
+++
@@ -1,7 +0,0 @@
-* Use Javascript to toggle menu
-* Insert social link
-* Update readme file
-* Update portfolio images, links and description
-* Use Javacript to fetch today and date
-* Enter data-itemid bla bla
-* Submit
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// VARIABLES
+
+const menu = document.querySelector(".menu__icon"),
+    responsiveNavList = document.querySelector(".nav__list"),
+    portfolioImages = document.querySelectorAll('.portfolio__item img'),
+    closePopupBtn = document.querySelector('#close__popup'),
+    popupContainer = document.querySelector('#popup__container');
+  
+
+
 // FUNCTIONS
 
 // Function to fetch current day
@@ -21,11 +31,37 @@ function hasChildWithClass(parentElement, className) {
     return parentElement.querySelector(`.${className}`) !== null;
 }
 
+// Function to dispay portfolio popup
+async function displayPortfolio(portfolio) {
+    const popupInfoContainer = document.querySelector('.popup__info');
 
-// VARIABLES
+    // Loading animation
+    popupInfoContainer.innerHTML = `<img style="width: 5rem" src="assets/images/loading.gif">`;
+    popupInfoContainer.classList.add('popup__animation');
 
-let menu = document.querySelector(".menu__icon"),
-    responsiveNavList = document.querySelector(".nav__list");
+    // Display popup
+    popupContainer.classList.remove('hidden');
+
+    setTimeout(()=>{
+        try {
+            // Removing loading animation style
+            popupInfoContainer.classList.remove('popup__animation');
+            // Generate DOM
+            popupInfoContainer.innerHTML = `
+                <h4>${portfolio.name}</h4>
+                <img src="${portfolio.image}" alt="">
+                <p>${portfolio.description}</p>
+            `
+        } catch {
+            // If errors
+            popupInfoContainer.innerHTML = `
+                <h4 class="popup__info__error">Sorry there was an error while fetching portfolio data</h4>
+                <img style="width: 15rem" src="assets/images/sorry.gif">
+            `;
+        }
+    }, 3000);
+}
+
 
 
 // EVENT LISTENERS
@@ -33,10 +69,10 @@ let menu = document.querySelector(".menu__icon"),
 // Event Listener for menu toggling
 menu.addEventListener("click", ()=>{
     if (hasChildWithClass(menu, 'fa-times')) {
-        responsiveNavList.style.right = "-100rem";
+        responsiveNavList.style.visibility = 'hidden';
         menu.innerHTML = `<i class="fa fa-bars fa-2x"></i>`;
     } else if (hasChildWithClass(menu, 'fa-bars')) {
-        responsiveNavList.style.right = "2rem";
+        responsiveNavList.style.visibility = 'visible';
         menu.innerHTML = `<i class="fa fa-times fa-2x"></i>`;
     }
 });
@@ -44,10 +80,38 @@ menu.addEventListener("click", ()=>{
 // Event Listener for clicking outside the menu to close it
 document.getElementsByTagName("main")[0].addEventListener("click", (event) => {
     if (hasChildWithClass(menu, 'fa-times')) {
-        responsiveNavList.style.right = "-100rem";
+        responsiveNavList.style.visibility = 'hidden';
         menu.innerHTML = `<i class="fa fa-bars fa-2x"></i>`;
     }
 });
+
+// Event Listener for portfolio image click
+portfolioImages.forEach((portfolioImage)=>{
+    portfolioImage.addEventListener('click', (e)=>{
+
+        // Getting parent element of ckicked image
+        const portfolioDiv = portfolioImage.parentElement;  // Can also use e.target.parentElement
+
+        // Extracting info from this portfolio
+        const portfolio = {
+            'name': portfolioDiv.querySelector('h3').textContent,
+            'description': portfolioDiv.querySelector('p').textContent,
+            'image': portfolioDiv.querySelector('img').src
+        };
+
+        // Displaying popup
+        displayPortfolio(portfolio);
+    })
+});
+
+// Event Listener to close popup
+closePopupBtn.addEventListener('click', ()=>{
+    console.log('Close button clicked');
+
+    // Hide popup
+    popupContainer.classList.add('hidden');
+})
+
 
 
 // CODE
