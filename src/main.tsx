1c5be347a21402541d0140447a8d691744789df6
Paulson Bosah <paulsonbosah@gmail.com>
2026-06-08 12:34:28 +0100

    Pages setup and Home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/css/App.css'
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
